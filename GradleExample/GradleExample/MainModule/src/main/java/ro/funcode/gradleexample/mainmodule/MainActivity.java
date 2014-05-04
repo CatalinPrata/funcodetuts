4f95ffe64682cfb923988172123fae32f2bad608
Catalin <prata.catalin@gmail.com>
2014-05-04 23:49:54 +0300

    added new example project, I will use this one for Gradle examples

---
+++ GradleExample/GradleExample/MainModule/src/main/java/ro/funcode/gradleexample/mainmodule/MainActivity.java
@@ -0,0 +1,52 @@
+package ro.funcode.gradleexample.mainmodule;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import ro.funcode.librarymodulea.librarymodulea.LibraryModuleAActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Open the library Activity
+                startActivity(new Intent(MainActivity.this, LibraryModuleAActivity.class));
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
